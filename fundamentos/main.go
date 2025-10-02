e5f8318e6696edfab0424f1bd3c31ec0b2bc3cd7
william viegas <williamviegas2@gmail.com>
2025-10-01 21:30:33 -0300

    Atualização: ajustes em fundamentos/main.go

--- fundamentos/main.go
+++ fundamentos/main.go
@@ -1,7 +1,16 @@
-package main 
+package main
+
+import "fmt"
+
+func somar(a int, b int) int {
+    return a + b
+}
+
+func imprimir(valor int) {
+    fmt.Println("Resultado:", valor)
+}
 
 func main() {
-	resultado := somar(3, 4) 
-	imprimir(resultado) 
-
-}
+    resultado := somar(3, 4)
+    imprimir(resultado)
+} 
